8c976c9e175bde758fe82d2f6e450830be9f915b
ayseugure <aayseugurr@gmail.com>
2021-03-11 14:06:48 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,18 @@
-#Explain your work
+#get a number from user to determine the upper number for the range in the homework
+#creating two lists including respectively odd numbers and even numbers from 0 to until this number
+#HW 1
+maxValue= int(input("Please enter number="))
+oddList, evenList=[], []
 
-#Question 1
-for x in range(a):
-	print(a)
+for x in range(maxValue+1):
+  if x%2==1:
+    oddList.append(x)
+  else:  
+    evenList.append(x)
+
+print("Odd Numbers : ",oddList)
+print("Even Numbers: ",evenList)
+even_copy=evenList.copy()
+even_copy.extend(oddList)
+even_copy.sort()
+print("All Numbers: ",even_copy)
